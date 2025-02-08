8d0dc58601b042e9302499d8ed3dba398fd34ac5
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2025-02-08 17:56:16 +0200

    Filter: get anchor tags with \'tip\' as the class name

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ url = "https://wakatime.com/leaders"
 response = requests.get(url)
 data = response.text
 soup = BeautifulSoup(data, 'html.parser')
-tags = soup.find_all('a')
+tags = soup.find_all('a', {'class':'tip'})
 
 for tag in tags:
     print(str(tag) + '\n')
