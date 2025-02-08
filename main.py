e1535407899d8e8ea1e46b9290bf3fad7f997ebc
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2025-02-08 18:10:00 +0200

    add an array of all the african countries

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
 
+african_countries = [
+    "Algeria", "Angola", "Benin", "Botswana", "Burkina Faso", "Burundi", 
+    "Cabo Verde", "Cameroon", "Central African Republic", "Chad", "Comoros", 
+    "Democratic Republic of the Congo", "Djibouti", "Egypt", "Equatorial Guinea", 
+    "Eritrea", "Eswatini", "Ethiopia", "Gabon", "Gambia", "Ghana", "Guinea", 
+    "Guinea-Bissau", "Ivory Coast", "Kenya", "Lesotho", "Liberia", "Libya", 
+    "Madagascar", "Malawi", "Mali", "Mauritania", "Mauritius", "Morocco", 
+    "Mozambique", "Namibia", "Niger", "Nigeria", "Republic of the Congo", 
+    "Rwanda", "Sao Tome and Principe", "Senegal", "Seychelles", "Sierra Leone", 
+    "Somalia", "South Africa", "South Sudan", "Sudan", "Tanzania", "Togo", 
+    "Tunisia", "Uganda", "Zambia", "Zimbabwe"
+]
+
 url = "https://wakatime.com/leaders"
 response = requests.get(url)
 data = response.text
